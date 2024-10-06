df7a239bd45deed4c8f077312f308b4f0d0dfb05
Vương Nguyễn Đình <nguyenvuong171104@gmail.com>
2024-10-06 15:44:15 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,408 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            btnNum1 = new Button();
+            btnNum2 = new Button();
+            btnNum3 = new Button();
+            btnNum4 = new Button();
+            btnNum5 = new Button();
+            btnNum6 = new Button();
+            btnNum7 = new Button();
+            btnNum8 = new Button();
+            button9 = new Button();
+            btnComma = new Button();
+            btnNum0 = new Button();
+            btnRemoveLast = new Button();
+            flowLayoutPanel2 = new FlowLayoutPanel();
+            btnAdd = new Button();
+            btnMinus = new Button();
+            btnMulti = new Button();
+            btnDiv = new Button();
+            btnEqual = new Button();
+            btnReset = new Button();
+            label1 = new Label();
+            flowLayoutPanel3 = new FlowLayoutPanel();
+            lblTemp = new Label();
+            lblResult = new Label();
+            flowLayoutPanel1.SuspendLayout();
+            flowLayoutPanel2.SuspendLayout();
+            flowLayoutPanel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.BorderStyle = BorderStyle.Fixed3D;
+            flowLayoutPanel1.Controls.Add(btnNum1);
+            flowLayoutPanel1.Controls.Add(btnNum2);
+            flowLayoutPanel1.Controls.Add(btnNum3);
+            flowLayoutPanel1.Controls.Add(btnNum4);
+            flowLayoutPanel1.Controls.Add(btnNum5);
+            flowLayoutPanel1.Controls.Add(btnNum6);
+            flowLayoutPanel1.Controls.Add(btnNum7);
+            flowLayoutPanel1.Controls.Add(btnNum8);
+            flowLayoutPanel1.Controls.Add(button9);
+            flowLayoutPanel1.Controls.Add(btnComma);
+            flowLayoutPanel1.Controls.Add(btnNum0);
+            flowLayoutPanel1.Controls.Add(btnRemoveLast);
+            flowLayoutPanel1.Location = new Point(14, 203);
+            flowLayoutPanel1.Margin = new Padding(4, 3, 4, 3);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(247, 302);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // btnNum1
+            // 
+            btnNum1.Font = new Font("Microsoft Sans Serif", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnNum1.Location = new Point(4, 3);
+            btnNum1.Margin = new Padding(4, 3, 4, 3);
+            btnNum1.Name = "btnNum1";
+            btnNum1.Size = new Size(64, 63);
+            btnNum1.TabIndex = 0;
+            btnNum1.Text = "1";
+            btnNum1.UseVisualStyleBackColor = true;
+            btnNum1.Click += btnNum1_Click;
+            // 
+            // btnNum2
+            // 
+            btnNum2.Font = new Font("Microsoft Sans Serif", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnNum2.Location = new Point(76, 3);
+            btnNum2.Margin = new Padding(4, 3, 4, 3);
+            btnNum2.Name = "btnNum2";
+            btnNum2.Size = new Size(64, 63);
+            btnNum2.TabIndex = 1;
+            btnNum2.Text = "2";
+            btnNum2.UseVisualStyleBackColor = true;
+            btnNum2.Click += btnNum2_Click;
+            // 
+            // btnNum3
+            // 
+            btnNum3.Font = new Font("Microsoft Sans Serif", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnNum3.Location = new Point(148, 3);
+            btnNum3.Margin = new Padding(4, 3, 4, 3);
+            btnNum3.Name = "btnNum3";
+            btnNum3.Size = new Size(64, 63);
+            btnNum3.TabIndex = 2;
+            btnNum3.Text = "3";
+            btnNum3.UseVisualStyleBackColor = true;
+            btnNum3.Click += btnNum3_Click;
+            // 
+            // btnNum4
+            // 
+            btnNum4.Font = new Font("Microsoft Sans Serif", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnNum4.Location = new Point(4, 72);
+            btnNum4.Margin = new Padding(4, 3, 4, 3);
+            btnNum4.Name = "btnNum4";
+            btnNum4.Size = new Size(64, 63);
+            btnNum4.TabIndex = 3;
+            btnNum4.Text = "4";
+            btnNum4.UseVisualStyleBackColor = true;
+            btnNum4.Click += btnNum4_Click;
+            // 
+            // btnNum5
+            // 
+            btnNum5.Font = new Font("Microsoft Sans Serif", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnNum5.Location = new Point(76, 72);
+            btnNum5.Margin = new Padding(4, 3, 4, 3);
+            btnNum5.Name = "btnNum5";
+            btnNum5.Size = new Size(64, 63);
+            btnNum5.TabIndex = 4;
+            btnNum5.Text = "5";
+            btnNum5.UseVisualStyleBackColor = true;
+            btnNum5.Click += btnNum5_Click;
+            // 
+            // btnNum6
+            // 
+            btnNum6.Font = new Font("Microsoft Sans Serif", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnNum6.Location = new Point(148, 72);
+            btnNum6.Margin = new Padding(4, 3, 4, 3);
+            btnNum6.Name = "btnNum6";
+            btnNum6.Size = new Size(64, 63);
+            btnNum6.TabIndex = 5;
+            btnNum6.Text = "6";
+            btnNum6.UseVisualStyleBackColor = true;
+            btnNum6.Click += btnNum6_Click;
+            // 
+            // btnNum7
+            // 
+            btnNum7.Font = new Font("Microsoft Sans Serif", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnNum7.Location = new Point(4, 141);
+            btnNum7.Margin = new Padding(4, 3, 4, 3);
+            btnNum7.Name = "btnNum7";
+            btnNum7.Size = new Size(64, 63);
+            btnNum7.TabIndex = 6;
+            btnNum7.Text = "7";
+            btnNum7.UseVisualStyleBackColor = true;
+            btnNum7.Click += btnNum7_Click;
+            // 
+            // btnNum8
+            // 
+            btnNum8.Font = new Font("Microsoft Sans Serif", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnNum8.Location = new Point(76, 141);
+            btnNum8.Margin = new Padding(4, 3, 4, 3);
+            btnNum8.Name = "btnNum8";
+            btnNum8.Size = new Size(64, 63);
+            btnNum8.TabIndex = 7;
+            btnNum8.Text = "8";
+            btnNum8.UseVisualStyleBackColor = true;
+            btnNum8.Click += btnNum8_Click;
+            // 
+            // button9
+            // 
+            button9.Font = new Font("Microsoft Sans Serif", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button9.Location = new Point(148, 141);
+            button9.Margin = new Padding(4, 3, 4, 3);
+            button9.Name = "button9";
+            button9.Size = new Size(64, 63);
+            button9.TabIndex = 8;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // btnComma
+            // 
+            btnComma.Font = new Font("Microsoft Sans Serif", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnComma.Location = new Point(4, 210);
+            btnComma.Margin = new Padding(4, 3, 4, 3);
+            btnComma.Name = "btnComma";
+            btnComma.Size = new Size(64, 63);
+            btnComma.TabIndex = 9;
+            btnComma.Text = ",";
+            btnComma.UseVisualStyleBackColor = true;
+            btnComma.Click += btnComma_Click;
+            // 
+            // btnNum0
+            // 
+            btnNum0.Font = new Font("Microsoft Sans Serif", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnNum0.Location = new Point(76, 210);
+            btnNum0.Margin = new Padding(4, 3, 4, 3);
+            btnNum0.Name = "btnNum0";
+            btnNum0.Size = new Size(64, 63);
+            btnNum0.TabIndex = 10;
+            btnNum0.Text = "0";
+            btnNum0.UseVisualStyleBackColor = true;
+            btnNum0.Click += btnNum0_Click;
+            // 
+            // btnRemoveLast
+            // 
+            btnRemoveLast.Font = new Font("Microsoft Sans Serif", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnRemoveLast.Location = new Point(148, 210);
+            btnRemoveLast.Margin = new Padding(4, 3, 4, 3);
+            btnRemoveLast.Name = "btnRemoveLast";
+            btnRemoveLast.Size = new Size(64, 63);
+            btnRemoveLast.TabIndex = 11;
+            btnRemoveLast.Text = "E";
+            btnRemoveLast.UseVisualStyleBackColor = true;
+            btnRemoveLast.Click += btnRemoveLast_Click;
+            // 
+            // flowLayoutPanel2
+            // 
+            flowLayoutPanel2.BorderStyle = BorderStyle.Fixed3D;
+            flowLayoutPanel2.Controls.Add(btnAdd);
+            flowLayoutPanel2.Controls.Add(btnMinus);
+            flowLayoutPanel2.Controls.Add(btnMulti);
+            flowLayoutPanel2.Controls.Add(btnDiv);
+            flowLayoutPanel2.Controls.Add(btnEqual);
+            flowLayoutPanel2.Controls.Add(btnReset);
+            flowLayoutPanel2.Controls.Add(label1);
+            flowLayoutPanel2.Location = new Point(268, 203);
+            flowLayoutPanel2.Margin = new Padding(4, 3, 4, 3);
+            flowLayoutPanel2.Name = "flowLayoutPanel2";
+            flowLayoutPanel2.Size = new Size(178, 302);
+            flowLayoutPanel2.TabIndex = 2;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Font = new Font("Microsoft Sans Serif", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnAdd.Location = new Point(4, 3);
+            btnAdd.Margin = new Padding(4, 3, 4, 3);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(79, 78);
+            btnAdd.TabIndex = 0;
+            btnAdd.Text = "+";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // btnMinus
+            // 
+            btnMinus.Font = new Font("Microsoft Sans Serif", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnMinus.Location = new Point(91, 3);
+            btnMinus.Margin = new Padding(4, 3, 4, 3);
+            btnMinus.Name = "btnMinus";
+            btnMinus.Size = new Size(79, 78);
+            btnMinus.TabIndex = 1;
+            btnMinus.Text = "-";
+            btnMinus.UseVisualStyleBackColor = true;
+            btnMinus.Click += btnMinus_Click;
+            // 
+            // btnMulti
+            // 
+            btnMulti.Font = new Font("Microsoft Sans Serif", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnMulti.Location = new Point(4, 87);
+            btnMulti.Margin = new Padding(4, 3, 4, 3);
+            btnMulti.Name = "btnMulti";
+            btnMulti.Size = new Size(79, 78);
+            btnMulti.TabIndex = 2;
+            btnMulti.Text = "*";
+            btnMulti.UseVisualStyleBackColor = true;
+            btnMulti.Click += btnMulti_Click;
+            // 
+            // btnDiv
+            // 
+            btnDiv.Font = new Font("Microsoft Sans Serif", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnDiv.Location = new Point(91, 87);
+            btnDiv.Margin = new Padding(4, 3, 4, 3);
+            btnDiv.Name = "btnDiv";
+            btnDiv.Size = new Size(79, 78);
+            btnDiv.TabIndex = 3;
+            btnDiv.Text = "/";
+            btnDiv.UseVisualStyleBackColor = true;
+            btnDiv.Click += btnDiv_Click;
+            // 
+            // btnEqual
+            // 
+            btnEqual.Font = new Font("Microsoft Sans Serif", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnEqual.Location = new Point(4, 171);
+            btnEqual.Margin = new Padding(4, 3, 4, 3);
+            btnEqual.Name = "btnEqual";
+            btnEqual.Size = new Size(79, 78);
+            btnEqual.TabIndex = 4;
+            btnEqual.Text = "=";
+            btnEqual.UseVisualStyleBackColor = true;
+            btnEqual.Click += btnEqual_Click;
+            // 
+            // btnReset
+            // 
+            btnReset.Font = new Font("Microsoft Sans Serif", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnReset.Location = new Point(91, 171);
+            btnReset.Margin = new Padding(4, 3, 4, 3);
+            btnReset.Name = "btnReset";
+            btnReset.Size = new Size(79, 78);
+            btnReset.TabIndex = 5;
+            btnReset.Text = "AC";
+            btnReset.UseVisualStyleBackColor = true;
+            btnReset.Click += btnReset_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Monotype Corsiva", 9.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(4, 252);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(76, 15);
+            label1.TabIndex = 6;
+            label1.Text = "@ By Vuong";
+            // 
+            // flowLayoutPanel3
+            // 
+            flowLayoutPanel3.BorderStyle = BorderStyle.FixedSingle;
+            flowLayoutPanel3.Controls.Add(lblTemp);
+            flowLayoutPanel3.Controls.Add(lblResult);
+            flowLayoutPanel3.FlowDirection = FlowDirection.TopDown;
+            flowLayoutPanel3.Location = new Point(14, 14);
+            flowLayoutPanel3.Margin = new Padding(4, 3, 4, 3);
+            flowLayoutPanel3.Name = "flowLayoutPanel3";
+            flowLayoutPanel3.Size = new Size(432, 182);
+            flowLayoutPanel3.TabIndex = 3;
+            // 
+            // lblTemp
+            // 
+            lblTemp.Font = new Font("Microsoft Sans Serif", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTemp.Location = new Point(4, 0);
+            lblTemp.Margin = new Padding(4, 0, 4, 0);
+            lblTemp.Name = "lblTemp";
+            lblTemp.Size = new Size(428, 89);
+            lblTemp.TabIndex = 0;
+            // 
+            // lblResult
+            // 
+            lblResult.BackColor = Color.Gray;
+            lblResult.Font = new Font("Microsoft Sans Serif", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblResult.ForeColor = SystemColors.ControlText;
+            lblResult.Location = new Point(4, 89);
+            lblResult.Margin = new Padding(4, 0, 4, 0);
+            lblResult.Name = "lblResult";
+            lblResult.Size = new Size(428, 89);
+            lblResult.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            BackColor = SystemColors.ActiveBorder;
+            ClientSize = new Size(460, 519);
+            Controls.Add(flowLayoutPanel3);
+            Controls.Add(flowLayoutPanel2);
+            Controls.Add(flowLayoutPanel1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(4, 3, 4, 3);
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Calculator";
+            flowLayoutPanel1.ResumeLayout(false);
+            flowLayoutPanel2.ResumeLayout(false);
+            flowLayoutPanel2.PerformLayout();
+            flowLayoutPanel3.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnNum1;
+        private System.Windows.Forms.Button btnNum2;
+        private System.Windows.Forms.Button btnNum3;
+        private System.Windows.Forms.Button btnNum4;
+        private System.Windows.Forms.Button btnNum5;
+        private System.Windows.Forms.Button btnNum6;
+        private System.Windows.Forms.Button btnNum7;
+        private System.Windows.Forms.Button btnNum8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button btnComma;
+        private System.Windows.Forms.Button btnNum0;
+        private System.Windows.Forms.Button btnRemoveLast;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnMulti;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Button btnEqual;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.Label lblTemp;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,342 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private double finalResult;
+        private string formular = "";
+        private string firstTemp = "";
+        private string secondTemp = "";
+        private string math = "";
+        private bool isPressMath = false;
+        private bool newCalculate = true;
+        private bool isPressEqual = false;
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+            UpdateLable();
+        }
+        private void Init()
+        {
+            finalResult = 0;
+        }
+        private void ResetValues()
+        {
+            finalResult = 0;
+            formular = "";
+            firstTemp = "";
+            secondTemp = "";
+            formular = "";
+            this.math = "";
+            isPressMath = false;
+            isPressEqual = false;
+            newCalculate = true;
+        }
+        private void Calculate(string math)
+        {
+            if (double.TryParse(firstTemp, out finalResult) && double.TryParse(secondTemp, out finalResult))
+            {
+                switch (math)
+                {
+                    case "+":
+                        {
+                            finalResult = double.Parse(firstTemp) + double.Parse(secondTemp);
+                            break;
+                        }
+                    case "-":
+                        {
+                            finalResult = double.Parse(firstTemp) - double.Parse(secondTemp);
+                            break;
+                        }
+                    case "*":
+                        {
+                            finalResult = double.Parse(firstTemp) * double.Parse(secondTemp);
+                            break;
+                        }
+                    case "/":
+                        {
+                            try
+                            {
+                                finalResult = double.Parse(firstTemp) / double.Parse(secondTemp);
+
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show("Cannot divice for 0!");
+
+                            }
+                            break;
+
+                        }
+
+                }
+            }
+
+
+            if (math == "")
+            {
+                if (double.TryParse(firstTemp, out finalResult))
+                {
+                    finalResult = double.Parse(firstTemp);
+
+                }
+            }
+
+
+
+            //firstTemp = finalResult.ToString();
+            firstTemp = "";
+            secondTemp = "";
+            formular = "";
+            this.math = "";
+            isPressMath = false;
+            isPressEqual = true;
+            newCalculate = false;
+        }
+        private void UpdateLable()
+        {
+            formular = firstTemp + math + secondTemp;
+            lblTemp.Text = formular;
+            lblResult.Text = finalResult.ToString();
+        }
+        private void UpdateTemp(string value)
+        {
+            if (isPressEqual)
+            {
+                newCalculate = true;
+                isPressEqual = false;
+            }
+            if (newCalculate)
+            {
+                if (!isPressMath)
+                {
+                    firstTemp += value;
+                }
+                else
+                {
+                    secondTemp += value;
+                }
+            }
+            else
+            {
+                secondTemp += value;
+            }
+        }
+        private void UpdateMath(string value)
+        {
+            if (isPressEqual)
+            {
+                newCalculate = false;
+            }
+            if (newCalculate)
+            {
+                if (!isPressMath)
+                {
+                    math = value;
+                    isPressMath = true;
+                }
+                else if (isPressMath && math != value)
+                {
+                    math = value;
+                    isPressMath = true;
+                }
+            }
+            else
+            {
+                firstTemp = finalResult.ToString();
+                if (!isPressMath)
+                {
+                    math = value;
+                    isPressMath = true;
+                }
+                else if (isPressMath && math != value)
+                {
+                    math = value;
+                    isPressMath = true;
+                }
+            }
+            UpdateLable();
+        }
+        // ==========================
+        private void btnNum1_Click(object sender, EventArgs e)
+        {
+            UpdateTemp("1");
+            UpdateLable();
+        }
+
+        private void btnNum2_Click(object sender, EventArgs e)
+        {
+            UpdateTemp("2");
+            UpdateLable();
+        }
+
+        private void btnNum3_Click(object sender, EventArgs e)
+        {
+            UpdateTemp("3");
+            UpdateLable();
+        }
+
+        private void btnNum4_Click(object sender, EventArgs e)
+        {
+            UpdateTemp("4");
+            UpdateLable();
+        }
+
+        private void btnNum5_Click(object sender, EventArgs e)
+        {
+            UpdateTemp("5");
+            UpdateLable();
+        }
+
+        private void btnNum6_Click(object sender, EventArgs e)
+        {
+            UpdateTemp("6");
+            UpdateLable();
+        }
+
+        private void btnNum7_Click(object sender, EventArgs e)
+        {
+            UpdateTemp("7");
+            UpdateLable();
+        }
+
+        private void btnNum8_Click(object sender, EventArgs e)
+        {
+            UpdateTemp("8");
+            UpdateLable();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            UpdateTemp("9");
+            UpdateLable();
+        }
+
+        private void btnComma_Click(object sender, EventArgs e)
+        {
+            if (!isPressMath)
+            {
+                if (!firstTemp.Contains("."))
+                {
+                    UpdateTemp(".");
+                    UpdateLable();
+                }
+            }
+            else
+            {
+                if (!secondTemp.Contains("."))
+                {
+                    UpdateTemp(".");
+                    UpdateLable();
+                }
+            }
+
+        }
+
+        private void btnNum0_Click(object sender, EventArgs e)
+        {
+            UpdateTemp("0");
+            UpdateLable();
+        }
+
+        private void btnRemoveLast_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!newCalculate)
+                {
+                    if (secondTemp != "")
+                    {
+                        secondTemp = secondTemp.Remove(secondTemp.Length - 1);
+                    }
+                    else
+                    {
+                        if (math != "")
+                        {
+                            math = math.Remove(math.Length - 1);
+                            isPressMath = false;
+                        }
+                    }
+                }
+                else
+                {
+                    if (secondTemp != "")
+                    {
+                        secondTemp = secondTemp.Remove(secondTemp.Length - 1);
+                    }
+                    else
+                    {
+                        if (math != "")
+                        {
+                            math = math.Remove(math.Length - 1);
+                            isPressMath = false;
+                        }
+                        else
+                        {
+                            if (firstTemp != "")
+                            {
+                                firstTemp = firstTemp.Remove(firstTemp.Length - 1);
+                            }
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Cannot delete with nothing!");
+            }
+            finally
+            {
+                UpdateLable();
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+            UpdateMath("+");
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+
+            UpdateMath("-");
+        }
+
+        private void btnMulti_Click(object sender, EventArgs e)
+        {
+
+            UpdateMath("*");
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+
+            UpdateMath("/");
+        }
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            double firstNumber;
+
+            double secondNumber;
+            if (double.TryParse(firstTemp, out firstNumber))
+            {
+                firstNumber = double.Parse(firstTemp);
+            }
+            if (double.TryParse(secondTemp, out secondNumber))
+            {
+                secondNumber = double.Parse(secondTemp);
+            }
+            Calculate(math);
+            UpdateLable();
+
+        }
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            ResetValues();
+            UpdateLable();
+        }
+    }
+}
